c8c034afd8f79d9f625f18b46d7e3bd315273a67
조대현 <jodaehyeon@jodaehyeon-ui-MacBookAir.local>
2023-12-03 21:50:07 +0900

    create simple_to_do_list

---
+++ lib/main.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> tileList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To Do List"),
+      ),
+      body: tileList.isEmpty
+          ? Center(child: Text("Make the List!"))
+          : ListView.builder(
+              itemCount: tileList.length,
+              itemBuilder: (context, index) {
+                var tile = tileList[index];
+                return ListTile(
+                  title: Text(
+                    tile["job"],
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: tile["isDone"] ? Colors.grey : Colors.black,
+                      decoration: tile["isDone"] ? TextDecoration.lineThrough : TextDecoration.none,
+                    ),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(CupertinoIcons.delete),
+                    onPressed: () {
+                      showDeleteDialog(
+                        context,
+                        index,
+                      );
+                    },
+                  ),
+                  onTap: () {
+                    setState(() {
+                      tile["isDone"] = !tile["isDone"];
+                    });
+                  },
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          String? job = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => CreatePage()),
+          );
+          if (job != null) {
+            setState(() {
+              tileList.add(
+                {
+                  "job": job,
+                  "isDone": false,
+                },
+              );
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  void showDeleteDialog(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Do you want to delete this tile?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  tileList.removeAt(index);
+                });
+                Navigator.pop(context);
+              },
+              child: Text(
+                "Yes",
+                style: TextStyle(color: Colors.pink),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class CreatePage extends StatefulWidget {
+  const CreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  TextEditingController textController = TextEditingController();
+
+  String? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Making Tile"),
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.chevron_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration: InputDecoration(
+                errorText: error,
+              ),
+            ),
+            SizedBox(height: 32),
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                child: Text(
+                  "add",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  String job = textController.text;
+                  if (job.isEmpty) {
+                    setState(() {
+                      error = "Please typing context.";
+                    });
+                  } else {
+                    setState(() {
+                      error = null;
+                    });
+                    Navigator.pop(context, job);
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
